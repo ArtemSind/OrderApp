d2861147f7ee9074d0e1de5eafcf98ef61fda377
Артем Синдаловский <sindalskij.as@edu.spbstu.ru>
2021-09-25 14:31:07 +0300

    Create repository class

---
+++ OrderApp/Models/Repository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderApp.Models
+{
+    interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Create(T item);
+        void Delete(int id);
+    }
+
+
+    public class OrderRepository : IRepository<Order>
+    {
+        private ApplicationContext db;
+
+        public OrderRepository(ApplicationContext context)
+        {
+            this.db = context;
+        }
+        public void Create(Order order)
+        {
+            db.Orders.Add(order);
+        }
+
+        public void Delete(int id)
+        {
+            Order order = db.Orders.Find(id);
+            if (order != null)
+                db.Orders.Remove(order);
+        }
+
+        public Order Get(int id)
+        {
+            return db.Orders.Find(id);
+        }
+
+        public IEnumerable<Order> GetAll()
+        {
+            return db.Orders;
+        }
+    }
+}
